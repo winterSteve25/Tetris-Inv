36971835f6773e2a4ef3c1349537f6e0e071deba
winterSteve25 <w1nter.ish3re@gmail.com>
2025-11-17 10:39:45 -0800

    .

--- Runtime/ItemType.cs
+++ Runtime/ItemType.cs
@@ -4,7 +4,7 @@ namespace TetrisInv.Runtime
 {
     public class ItemType : ScriptableObject
     {
-        [field: SerializeField] public int StackSize { get; private set; }
-        [field: SerializeField] public Vector2Int Size { get; private set; }
+        [field: SerializeField] public virtual int StackSize { get; protected set; }
+        [field: SerializeField] public virtual Vector2Int Size { get; protected set; }
     }
 }
